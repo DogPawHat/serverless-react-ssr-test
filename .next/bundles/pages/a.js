7690d1553244fecc8e98f41189587ae9c5c85dc9
Ciarán Curley <ciaran.curley.11.11@gmail.com>
2017-12-14 14:42:44 +0000

    remove .next folder

--- .next/bundles/pages/a.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/a', function() {
-            var comp = module.exports=webpackJsonp([6],{188:function(e,t,u){e.exports=u(189)},189:function(e,t,u){"use strict";function l(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var n=u(3),a=l(n),r=u(45),d=(l(r),u(70)),f=l(d);t.default=function(){return a.default.createElement("div",null,a.default.createElement(f.default,null),a.default.createElement("h1",null,"This is page A"))}}},[188]);
-            return { page: comp.default }
-          })
-        
--- .next/bundles/pages/b.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/b', function() {
-            var comp = module.exports=webpackJsonp([5],{201:function(e,t,u){e.exports=u(202)},202:function(e,t,u){"use strict";function l(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var n=u(3),a=l(n),r=u(45),d=(l(r),u(70)),f=l(d);t.default=function(){return a.default.createElement("div",null,a.default.createElement(f.default,null),a.default.createElement("h1",null,"This is page B"))}}},[201]);
-            return { page: comp.default }
-          })
-        
